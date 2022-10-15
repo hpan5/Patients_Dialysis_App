8d52f93afcfdeaa31ff4c1822ebd0ea19634848a
Doris <hpan996@gmail.com>
2022-10-15 12:01:01 -0700

    update packages in frontend

--- patient-frontend-app/src/App.js
+++ patient-frontend-app/src/App.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import './App.css';
 import PatientsList from './feature/PatientsList';
 
