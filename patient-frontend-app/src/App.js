72ca41d203c3fe4ce45b42735218d47d20a6162a
Doris <hpan996@gmail.com>
2022-10-15 12:31:18 -0700

    install mui

--- patient-frontend-app/src/App.js
+++ patient-frontend-app/src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Redirect,
 } from 'react-router-dom'
 import './App.css';
 import PatientsList from './feature/PatientsList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello patients Dialysis App</h1>
-      <PatientsList />
-    </div>
+    <Router>
+      <div className="App">
+        <PatientsList />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <PatientsList />
+              </React.Fragment>
+            )}
+          />
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
+/*
 
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+*/
 export default App;
